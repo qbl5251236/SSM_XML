5cb169389c48fbe13cf5cee871e455eae2b6b214
xiaoqiuxx <3253530035@qq.com>
2022-02-27 10:39:32 +0800

    hello

---
+++ src/main/java/com/xiaoqiu/controller/Hello.java
@@ -0,0 +1,9 @@
+package com.xiaoqiu.controller;
+
+//@Time : 2022/2/27 10:38
+//@Author : xiaoqiuxx
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
