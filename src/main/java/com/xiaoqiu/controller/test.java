7955d41dbbb90a1062a5c811d57acd36810b20ca
xiaoqiuxx <3253530035@qq.com>
2022-02-27 10:35:54 +0800

    hello

--- src/main/java/com/xiaoqiu/controller/test.java
+++
@@ -1,9 +0,0 @@
-package com.xiaoqiu.controller;
-
-//@Time : 2022/2/26 17:53
-//@Author : xiaoqiuxx
-public class test {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
